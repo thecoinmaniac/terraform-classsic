72b9a27316ed294a63a04af2b2c2712ed74faddd
Goutham <arun.goutham@capgemini.com>
2019-08-07 11:14:24 +0530

    2

--- main.tf
+++ main.tf
@@ -23,3 +23,27 @@ module "vpc" {
   vpc-public-subnet-cidr = ["10.11.1.0/24","10.11.2.0/24","10.11.3.0/24"]
   vpc-private-subnet-cidr = ["10.11.4.0/24","10.11.5.0/24","10.11.6.0/24"]
 }
+
+module "barclays-bastion-sg" {
+  source = "./modules/sg-bastion"
+  region = "us-east-2"
+  vpc-id = "${module.vpc.vpc-id}"
+  ec2-sg-name = "bastion-sg"
+
+  ###SECURITY INBOUND GROUP RULES###
+  #RULE-1-INBOUND-RULES
+  rule-1-from-port = 22
+  rule-1-protocol = "tcp"
+  rule-1-to-port = 22
+  rule-1-cidr_blocks = "0.0.0.0/0"
+
+
+  ###SECURITY GROUP OUTBOUND RULES###
+  #RULE-1-OUTBOUND-RULES
+  outbound-rule-1-from-port = 0
+  outbound-rule-1-protocol = "-1"
+  outbound-rule-1-to-port = 0
+  outbound-rule-1-cidr_blocks = "0.0.0.0/0"
+
+  #NOTE: ONLY ALL PORTS WILL BE "" & CIDR BLOCK WILL IN COMMAS ""
+}
